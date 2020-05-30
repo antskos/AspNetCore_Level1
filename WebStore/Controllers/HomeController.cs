70574649ded93159dbce5dc51e99b6bd162f7056
antskos <antskos@gmail.com>
2020-05-30 19:34:42 +0300

    добавление действий в контроллер home добавление представления Login

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -10,5 +10,20 @@ namespace WebStore.Controllers
     {
         public IActionResult Index() => View();
 
+        public IActionResult Blog() => View();
+
+        public IActionResult BlogSingle() => View();
+
+        public IActionResult Cart() => View();
+
+        public IActionResult CheckOut() => View();
+
+        public IActionResult ContactUs() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult ProductDetails() => View();
+
+        public IActionResult Shop() => View();
     }
 }
