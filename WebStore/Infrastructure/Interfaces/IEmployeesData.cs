8a6b56c3104e7897fa6eb791ef4fd3c26e8bc426
Anton Skosyrskiy <antskos@gmail.com>
2020-06-01 15:53:28 +0300

    Добавление интерфейса управления записями сотрудников

---
+++ WebStore/Infrastructure/Interfaces/IEmployeesData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Infrastructure.Interfaces
+{
+    public interface IEmployeesData
+    {
+        IEnumerable<Employee> Get();
+
+        Employee GetById(int id);
+
+        int Add(Employee emp);
+
+        void Edit(Employee emp);
+
+        bool Delete(int id);
+
+        void SaveChanges();
+        
+    }
+}
