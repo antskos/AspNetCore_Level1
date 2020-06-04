f1301a65b1cdcaf8791e8a4ec000889c1ef25e93
antskos <antskos@gmail.com>
2020-06-05 00:37:55 +0300

    Добавление в сервис интерфейса получения  продукта  и его реализация в классе InMemoryProductData.

--- WebStore/Infrastructure/Services/InMemoryProductData.cs
+++ WebStore/Infrastructure/Services/InMemoryProductData.cs
@@ -2,6 +2,7 @@
 using WebStore.Domain.Entities;
 using System.Collections.Generic;
 using WebStore.Data;
+using System.Linq;
 
 namespace WebStore.Infrastructure.Services
 {
@@ -10,5 +11,18 @@ namespace WebStore.Infrastructure.Services
         public IEnumerable<Brand> GetBrands() => TestData.Brands;
 
         public IEnumerable<Section> GetSections() => TestData.Sections;
+
+        public IEnumerable<Product> GetProducts(ProductFilter filter = null)
+        {
+            var query = TestData.Products;
+
+            if (filter?.SectionId != null)
+                query = query.Where(product => product.SectionId == filter.SectionId);
+
+            if (filter?.BrandId != null)
+                query = query.Where(product => product.BrandId == filter.BrandId);
+
+            return query;
+        }
     }
 }
