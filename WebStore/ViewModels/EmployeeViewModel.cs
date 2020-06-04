a2236ccb6ae070db8b080f38594055e1913df47b
Anton Skosyrskiy <antskos@gmail.com>
2020-06-04 14:44:09 +0300

    Добавление в EmployeeViewModel  атрибутов валидации и аннотации данных

--- WebStore/ViewModels/EmployeeViewModel.cs
+++ WebStore/ViewModels/EmployeeViewModel.cs
@@ -1,20 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace WebStore.ViewModels
 {
-    public class EmployeeViewModel
+    public class EmployeeViewModel //: IValidatableObject
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
 
+        [Display(Name = "Имя")]
+        [Required(ErrorMessage = "Поле 'Имя' обязательно для заполнения")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Имя должно быть длинной от 2 до 100 символов")]
         public string Name { get; set; }
 
+        [Display(Name = "Фамилия")]
+        [Required(ErrorMessage = "Поле 'Фамилия' обязательно для заполнения")]
+        [MinLength(2, ErrorMessage = "Фамилия должна быть длинной не менее 2 символов")]
         public string Surname { get; set; }
 
+        [Display(Name = "Отчество")]
         public string Patronymic { get; set; }
 
+        [Display(Name = "Возраст")]
+        [Required]
+        [Range(18,150,ErrorMessage = "Возраст должен быть в пределах от 18 до 150 лет"])
         public int Age { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+            
+        //}
     }
 }
