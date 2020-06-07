9d6a2508ed9292881f9f2bcb9be45ea5aed9736d
antskos <antskos@gmail.com>
2020-06-07 16:16:18 +0300

    Добавление атрибутов и изменений в классы сущностей

--- WebStore.Domain/Entities/Section.cs
+++ WebStore.Domain/Entities/Section.cs
@@ -1,13 +1,20 @@
 ﻿using WebStore.Domain.Entities.Base.Interfaces;
 using WebStore.Domain.Entities.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
 
 
 namespace WebStore.Domain.Entities
 {
+    [Table("ProductSection")]
     public class Section : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
 
         public int? ParentId { get; set; }
+
+        public virtual Section ParentSection { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
     }
 }
