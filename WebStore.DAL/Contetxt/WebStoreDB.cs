9676db9e93f44d50b49437b30d404cb165956cf3
antskos <antskos@gmail.com>
2020-06-09 22:07:35 +0300

    Создание проекта для базы данных

---
+++ WebStore.DAL/Contetxt/WebStoreDB.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.DAL.Contetxt
+{
+    public class WebStoreDB : DbContext
+    {
+    }
+}
