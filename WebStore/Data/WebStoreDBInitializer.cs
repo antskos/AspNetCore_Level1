5d243e601f6d7db2cc7927c301eddd9aedb5e2d3
Anton Skosyrskiy <antskos@gmail.com>
2020-06-15 22:06:52 +0300

    Внесение тестовых сотрудников при их отстутсвии в базе

--- WebStore/Data/WebStoreDBInitializer.cs
+++ WebStore/Data/WebStoreDBInitializer.cs
@@ -61,6 +61,24 @@ namespace WebStore.Data
 
                 transaction.Commit();
             }
+
+            if (!_db.Employees.Any())
+                using (var transaction = db.BeginTransaction())
+                {
+                    var employees = TestData.Employees.ToList();
+                    //foreach (var emp in employees)
+                    //{
+                    //    emp.Id = 0;
+                    //}
+                    employees.ForEach(emp => emp.Id = 0);
+                    _db.Employees.AddRange(employees);
+
+                    db.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Employees] ON");
+                    _db.SaveChanges();
+                    db.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[Employees] OFF");
+
+                    transaction.Commit();
+                }
         }
     }
 }
