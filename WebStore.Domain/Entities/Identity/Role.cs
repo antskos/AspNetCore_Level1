1cf7fe4f2fe5f133b02f2f8d8f2313757c14548b
Anton Skosyrskiy <antskos@gmail.com>
2020-06-21 19:29:37 +0300

    Инициализация пользователя Администратор и ролей администратор и пользователь

--- WebStore.Domain/Entities/Identity/Role.cs
+++ WebStore.Domain/Entities/Identity/Role.cs
@@ -4,6 +4,7 @@ namespace WebStore.Domain.Entities.Identity
 {
     public class Role : IdentityRole
     {
-        public const string Administrator = "Administrators";
+        public const string Administrator = "Administrator";
+        public const string User = "User";
     }
 }
--- WebStore/Data/WebStoreDBInitializer.cs
+++ WebStore/Data/WebStoreDBInitializer.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.DAL.Contetxt;
+using WebStore.Domain.Entities.Identity;
 
 namespace WebStore.Data
 {
@@ -12,9 +14,17 @@ namespace WebStore.Data
     public class WebStoreDBInitializer
     {
         private readonly WebStoreDB _db;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<Role> _roleManager;
 
-        public WebStoreDBInitializer(WebStoreDB db) => _db = db;
-        
+        public WebStoreDBInitializer(WebStoreDB db, UserManager<User> userManager, RoleManager<Role> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+
         public void Initialize() 
         {
             var db = _db.Database;
@@ -22,10 +32,19 @@ namespace WebStore.Data
             // проверка создаётся ли новая БД
             //if (db.EnsureDeleted())
             //    if (!db.EnsureCreated())
-            //        throw new InvalidOperationException("Ошибка при создании базы данных товаров");
-
+            //        throw new InvalidOperationException("Ошибка при создании базы данных!");
             db.Migrate();
 
+            InitializeProducts();
+            InitializeEmployees();
+            InitializeIdentityAsync().Wait();
+
+        } //Initialize() method
+
+        private void InitializeProducts()
+        {
+            var db = _db.Database;
+            
             // если есть товары в базе, то она проинициализирована
             if (_db.Products.Any()) return;
 
@@ -61,22 +80,49 @@ namespace WebStore.Data
 
                 transaction.Commit();
             }
+        }  //InitializeProducts() method
 
-            if (!_db.Employees.Any())
-                using (var transaction = db.BeginTransaction())
+        private void InitializeEmployees() 
+        {
+            if (_db.Employees.Any()) return;
+            
+            using (var transaction = _db.Database.BeginTransaction())
+            {
+                var employees = TestData.Employees.ToList();
+                    
+                employees.ForEach(emp => emp.Id = 0);
+                _db.Employees.AddRange(employees);
+
+                _db.SaveChanges();
+                transaction.Commit();
+            }
+        }   //InitializeEmployees() method
+
+        private async Task InitializeIdentityAsync() 
+        {
+            // проверка определены ли в БД "обязательные" роли и если нет создаём их
+            if (!await _roleManager.RoleExistsAsync(Role.Administrator))
+                await _roleManager.CreateAsync(new Role { Name = Role.Administrator });
+
+            if (!await _roleManager.RoleExistsAsync(Role.User))
+                await _roleManager.CreateAsync(new Role { Name = Role.User });
+
+            if (await _userManager.FindByNameAsync(User.Administrator) is null) 
+            {
+                var admin = new User(){ UserName = User.Administrator };
+
+                var create_result = await _userManager.CreateAsync(admin, User.DefaultAdminPassword);
+                if (create_result.Succeeded)
+                    await _userManager.AddToRoleAsync(admin, Role.Administrator);
+                else 
                 {
-                    var employees = TestData.Employees.ToList();
-                    //foreach (var emp in employees)
-                    //{
-                    //    emp.Id = 0;
-                    //}
-                    employees.ForEach(emp => emp.Id = 0);
-                    _db.Employees.AddRange(employees);
-
-                    _db.SaveChanges();
-
-                    transaction.Commit();
+                    var errors = create_result.Errors.Select(e => e.Description);
+                    throw new InvalidOperationException($"Ошибка при создании пользователя с правами администратора: " +
+                        $"{string.Join(",", errors)}");
                 }
-        }
+            }
+
+
+        }   //InitializePdentity() method
     }
 }
