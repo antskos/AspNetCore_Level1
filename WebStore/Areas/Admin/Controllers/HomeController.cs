26bd3cc733219838957dd91bbe284b201699aab8
Anton Skosyrskiy <antskos@gmail.com>
2020-06-27 18:40:29 +0300

    Заготовка области администратора

---
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Areas.Admin.Controllers
+{
+    // контролерам в областях надо в атрибуте указывать принадлежность к области, для исключения неоднозначности
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
