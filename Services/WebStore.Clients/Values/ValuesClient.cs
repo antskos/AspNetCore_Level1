5e08089281951a52f666a7a566f7a24180fe5963
antskos@gmail.com <antskos@gmail.com>
2020-09-24 18:19:15 +0300

    Клиент для тестового WebApi

---
+++ Services/WebStore.Clients/Values/ValuesClient.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using WebStore.Clients.Base;
+using WebStore.Interfaces.TestAPI;
+
+namespace WebStore.Clients.Values
+{
+     public class ValuesClient : BaseClient, IValueService
+    {
+        public ValuesClient(IConfiguration configuration) : base(configuration, "api/values") {}
+
+        public HttpStatusCode Delete(int id)
+        {
+            var response = _client.DeleteAsync($"{_serviceAddress}/{id}").Result;
+            return response.StatusCode;
+        }
+
+        public IEnumerable<string> Get()
+        {
+            var response = _client.GetAsync(_serviceAddress).Result;
+            if (response.IsSuccessStatusCode)
+                return response.Content.ReadAsAsync<IEnumerable<string>>().Result;
+            else
+                return Enumerable.Empty<string>();
+
+        }
+
+        public string Get(int id)
+        {
+            var response = _client.GetAsync($"{_serviceAddress}/{id}").Result;
+            if (response.IsSuccessStatusCode)
+                return response.Content.ReadAsAsync<string>().Result;
+            else
+                return string.Empty;
+        }
+
+        public Uri Post(string value)
+        {
+            var response = _client.PostAsJsonAsync(_serviceAddress, value).Result;
+            return response.EnsureSuccessStatusCode().Headers.Location;
+        }
+
+        public HttpStatusCode Update(int id, string value)
+        {
+            var response = _client.PutAsJsonAsync($"{_serviceAddress}/{id}", value).Result;
+            return response.EnsureSuccessStatusCode().StatusCode;
+        }
+    }
+}
--- Services/WebStore.ServiceHosting/Controllers/ValuesController.cs
+++ Services/WebStore.ServiceHosting/Controllers/ValuesController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebStore.ServiceHosting.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/values")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
