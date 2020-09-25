86b7a60cb358d0f92b4912359aad2ee18e2c383a
antskos@gmail.com <antskos@gmail.com>
2020-09-25 10:49:54 +0300

    Подключение сервисов бизнес-логики к проекту ServiceHosting

--- Services/WebStore.ServiceHosting/Startup.cs
+++ Services/WebStore.ServiceHosting/Startup.cs
@@ -1,29 +1,42 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using WebStore.DAL.Contetxt;
+using WebStore.Interfaces.Services;
+using WebStore.Services.Data;
+using WebStore.Services.Products;
+using WebStore.Services.Products.InCookies;
+using WebStore.Services.Products.InSQL;
 
 namespace WebStore.ServiceHosting
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<WebStoreDB>(opt =>
+                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<WebStoreDBInitializer>();
+
+            //services.AddScoped<IProductData, SqlProductData>();
+            //services.AddScoped<IEmployeesData, SqlEmployeesData>();
+            //services.AddScoped<ICartService, CookiesCartService>();
+            //services.AddScoped<IOrderService, SqlOrderService>();
+
+            services.AddWebStoreServices();
+
+            // вспомогательный сервис дл€ сервиса корзины
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
             services.AddControllers();
         }
 
