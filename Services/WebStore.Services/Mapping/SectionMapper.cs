9cce39cfe71283a160f7bdbdedba34184d921f7e
antskos@gmail.com <antskos@gmail.com>
2020-09-26 21:43:23 +0300

    Маппинг в DTO и обратно

--- Services/WebStore.Services/Mapping/SectionMapper.cs
+++ Services/WebStore.Services/Mapping/SectionMapper.cs
@@ -1,13 +1,20 @@
-﻿using AutoMapper.Configuration.Annotations;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using WebStore.Domain.DTO.Products;
+﻿using WebStore.Domain.DTO.Products;
+using WebStore.Domain.Entities;
 
 namespace WebStore.Services.Mapping
 {
     public static class SectionMapper
     {
-        public static SectionDTO
+        public static SectionDTO ToDTO(this Section section) => section is null ? null : new SectionDTO
+        {
+            Id = section.Id,
+            Name = section.Name
+        };
+
+        public static Section FromDTO(this SectionDTO section) => section is null ? null : new Section
+        {
+            Id = section.Id,
+            Name = section.Name
+        };
     }
 }
