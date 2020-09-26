966bba1584d511a53910a230c92c870766e25baf
antskos@gmail.com <antskos@gmail.com>
2020-09-26 20:52:13 +0300

    DTO  модели Brand,Section, Product

---
+++ Services/WebStore.Services/Mapping/SectionMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper.Configuration.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.DTO.Products;
+
+namespace WebStore.Services.Mapping
+{
+    public static class SectionMapper
+    {
+        public static SectionDTO
+    }
+}
