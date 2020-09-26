b6c5f30385d17dee5a1dd94244383abf9756ad02
antskos@gmail.com <antskos@gmail.com>
2020-09-27 01:55:22 +0300

    Клиент WebApi Orders

---
+++ Services/WebStore.Clients/Orders/OrdersClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+using WebStore.Domain;
+using WebStore.Domain.DTO.Orders;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Clients.Orders
+{
+    public class OrdersClient : BaseClient, IOrderService
+    {
+        public OrdersClient(IConfiguration configuration) : base(configuration, WebAPI.Orders) { }
+
+        public async Task<IEnumerable<OrderDTO>> GetUserOrders(string userName) =>
+            await GetAsync<IEnumerable<OrderDTO>>($"{_serviceAddress}/user/{userName}");
+
+        public async Task<OrderDTO> GetOrderById(int id) =>
+            await GetAsync<OrderDTO>($"{_serviceAddress}/{id}");
+
+        public async Task<OrderDTO> CreateOrder(string userName, CreateOrderModel orderModel)
+        {
+            var response = await PostAsync($"{_serviceAddress}/{userName}", orderModel);
+            return await response.Content.ReadAsAsync<OrderDTO>();
+        }
+    }
+}
