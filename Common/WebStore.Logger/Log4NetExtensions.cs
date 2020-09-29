07b39d34694b3ddcee4734bc7c360ad6bd66639b
antskos@gmail.com <antskos@gmail.com>
2020-09-30 00:37:48 +0300

    Логгер на основе библиотеки Log4Net

---
+++ Common/WebStore.Logger/Log4NetExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace WebStore.Logger
+{
+    public static class Log4NetExtensions
+    {
+        public static ILoggerFactory AddLog4Net(this ILoggerFactory factory, string configurationFile = "log4net.config")
+        {
+            if (Path.IsPathRooted(configurationFile))
+            {
+                var assembly = Assembly.GetEntryAssembly() ?? throw new InvalidOperationException("Не удалось определить сборку с точкой входа в приложение");
+                var dir = Path.GetDirectoryName(assembly.Location) ?? throw new InvalidOperationException("Не удалось определить путь размещения сборки с точкой входа в приложение");
+                configurationFile = Path.Combine(dir, configurationFile);
+            }
+
+            factory.AddProvider(new Log4NetLoggerProvider(configurationFile));
+
+            return factory;
+        }
+    }
+}
