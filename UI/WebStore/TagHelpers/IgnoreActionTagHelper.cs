7837eef13c106328929d57d3c90f97564c7cb774
antskos@gmail.com <antskos@gmail.com>
2020-10-10 20:50:23 +0300

    Создание tag-helper для подсвечивания ссылок за счёт применения class="active"

---
+++ UI/WebStore/TagHelpers/IgnoreActionTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebStore.TagHelpers
+{
+    //[HtmlTargetElement(Attributes = AttributeName)]
+    public class IgnoreActionTagHelper : TagHelper
+    {
+        //public const string AttributeName = "ignore-action";  // имя тэга
+
+
+        //[HtmlAttributeName("asp-controller")]
+        //public string Controller { get; set; }
+    }
+}
