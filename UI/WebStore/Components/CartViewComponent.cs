292354db994b52007572a393f5c7809caf5b0d57
antskos@gmail.com <antskos@gmail.com>
2020-10-18 00:00:24 +0300

    Добавлен ViewComponent Корзина(Cart)

---
+++ UI/WebStore/Components/CartViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly ICartService _cartService;
+
+        public CartViewComponent(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        
+
+        public IViewComponentResult Invoke() => View(_cartService.TransformFromCart());
+    }
+}
